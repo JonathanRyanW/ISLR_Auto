bc280081869dc0deee59c6825a6a39ae40fe74e8
Jonathan Ryan <jonathanryan576@gmail.com>
2021-03-12 10:28:55 +0700

    To do list.

---
+++ script/to do list.R
@@ -0,0 +1,10 @@
+"To do:
+1. Create the plots between the 4 positively correlated variables!
+2. EDA on every other variable
+3. EDA on year
+3. KNN with only the strong predictors.
+
+Questions:
+1. Can we make a better KNN model? We only get 76% accuracy with this one.
+
+"
